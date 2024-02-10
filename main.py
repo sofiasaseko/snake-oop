6a096edf263438414fc2e964c1fa89b32be55d64
sofiasaseko <sofiasaseko@gmail.com>
2024-02-10 15:00:40 +0300

    fix: turtle paint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from turtle import *
 hideturtle()
-speed(10)
 t1= Turtle()
 t2= Turtle()
 t3=Turtle()
@@ -8,10 +7,10 @@ t3=Turtle()
 t1.color('red')
 t2.color('green')
 t3.color('blue')
-t2.left(162)
-t3.left(270)
+t2.left(120)
+t3.left(240)
 
-for i in range(28):
+for i in range(36):
     t1.forward(10)
     t1.left(10)
 
